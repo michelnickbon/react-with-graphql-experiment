075eae81006e10bc98c2f81fccace88e7c7057db
Michel Nickbon <michel.lemponen@gmail.com>
2020-10-02 16:13:45 +0200

    Refactored tests, mocked api call

--- src/__tests___/GraphQLComponentTest.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import GraphQLComponent from '../components/GraphQLComponent';
-
-test('Snapshot is rendered', () => {
-    const component = renderer.create(<GraphQLComponent />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
---
+++ src/__tests___/ResultComponent.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Result from "../components/Result"
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+// Render component
+test("Result component is receiving and displaying API data", async () => {
+
+  // Mock API data
+  const GetGraphQLDataMock = jest.fn();
+  const resolvedValue = {"data":"Country"};
+  GetGraphQLDataMock.mockResolvedValue(resolvedValue);
+  const response = await GetGraphQLDataMock()
+
+  // Mount
+  const wrapper = shallow(<Result result={response}/>);
+  const props = wrapper.props().children;
+
+  // Validate
+  expect(typeof props).toBe('object');
+  expect(props).toBe(resolvedValue);
+
+});
